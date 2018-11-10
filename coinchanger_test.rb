47ab1bc1c2cb5136994041ca1921b04cc3216000
Steven <ekczile@gmail.com>
2018-11-09 21:15:09 -0500

    2 quarters, 2 dimes pass

--- coinchanger_test.rb
+++ coinchanger_test.rb
@@ -9,10 +9,13 @@ class Testallconversions < Minitest::Test
     def test_for_1_quarter
         assert_equal({:quarter => 1}, coin_counter(25))
     end 
-    def test_for_2_pennys
+    def test_for_2_quarters
         assert_equal({:quarter => 2}, coin_counter(50))
     end
-    def test_for_1_queater_1_dime
-    assert_equal({:quarter=>1, :dime=>1}, coin_counter(35))
+    def test_for_1_quarter_1_dime
+        assert_equal({:quarter=> 1, :dime=> 1}, coin_counter(35))
+    end
+    def test_for_2_quarters_2_dimes
+        assert_equal({:quarter=> 2, :dime=> 2}, coin_counter(70))
     end
 end 
