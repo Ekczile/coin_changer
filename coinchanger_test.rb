e13fa0b04bf43ec89163261f86d85df01248251e
Steven <ekczile@gmail.com>
2018-11-09 20:49:26 -0500

    passing 2 quarters

--- coinchanger_test.rb
+++ coinchanger_test.rb
@@ -7,9 +7,9 @@ class Testallconversions < Minitest::Test
         assert_equal(1,1)
     end
     def test_for_1_quarter
-        assert_equal({:quarter => 1}, coin_counter(35))
+        assert_equal({:quarter => 1}, coin_counter(25))
     end 
-    # def test_for_2_pennys
-    #     assert_equal("", coin_counter(2))
-    # end
+    def test_for_2_pennys
+        assert_equal({:quarter => 2}, coin_counter(50))
+    end
 end
