8dae0124e6fb6f8300dc1af4a5f8cb0cb413ede4
Steven <ekczile@gmail.com>
2018-11-09 21:37:52 -0500

    first big number passes

--- coinchanger_test.rb
+++ coinchanger_test.rb
@@ -27,4 +27,7 @@ class Testallconversions < Minitest::Test
     def test_for_2_quarter_1_dime__1_nickle_and_4_pennys
         assert_equal({:quarter => 2, :dime => 1, :nickle => 1, :pennys => 4},coin_counter(69))
     end
+    def test_first_big_num
+        assert_equal({:quarter=>108000},coin_counter(2700000))
+    end
 end 
