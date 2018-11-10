37da587fb681607c3a4e490184a1e9bd8728607d
Steven <ekczile@gmail.com>
2018-11-09 20:36:26 -0500

    changed logic doing quarter first

--- coinchanger.rb
+++ coinchanger.rb
@@ -1,6 +1,7 @@
 def coin_counter(coins)
-    h = {
-        1 => "penny"
-    }
-    return h.fetch(coins)
+    hash = Hash.new
+    quarter = coins / 25
+    hash.merge!(:quarter => quarter)
+    p hash
+    return hash
 end
--- coinchanger_test.rb
+++ coinchanger_test.rb
@@ -6,7 +6,10 @@ class Testallconversions < Minitest::Test
     def test_1
         assert_equal(1,1)
     end
-    def test_for_1_penny
-        assert_equal("penny", coin_counter(1))
-    end
+    def test_for_1_quarter
+        assert_equal({:quarter => 1}, coin_counter(35))
+    end 
+    # def test_for_2_pennys
+    #     assert_equal("", coin_counter(2))
+    # end
 end
